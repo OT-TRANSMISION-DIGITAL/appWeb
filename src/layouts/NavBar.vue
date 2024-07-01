94d003a2f894c058c7b771176b53a3bbd2cac2fe
Elnumer01 <eromario.marve@gmail.com>
2024-07-01 14:52:02 -0600

    logout
    implementacion de la api de logout

--- src/layouts/NavBar.vue
+++ src/layouts/NavBar.vue
@@ -19,7 +19,7 @@
                 {{ correo }}
               </div>
               <hr>
-              <fwb-list-group-item class="cursor-pointer">
+              <fwb-list-group-item class="cursor-pointer" @click="logout()">
                 Cerrar Sesión
               </fwb-list-group-item>
             </fwb-list-group>
@@ -41,6 +41,9 @@
     FwbNavbarLink,
     FwbNavbarLogo,
   } from 'flowbite-vue'
+
+import axios from 'axios'
+
   import IMGSS from '../assets/OT_logo_light.jpeg'
   import { ref } from 'vue';
 
@@ -48,4 +51,28 @@
   const user = JSON.parse(localStorage.getItem('user'));
   const name = ref(user?.nombre || '');
   const correo = ref(user?.correo || '');
+  const succ = ref(false)
+  const URL_API = import.meta.env.VITE_API_URL
+
+  const logout = async () => {
+    try {
+
+      const response  = await axios.get(`${URL_API}/logout`,{
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + localStorage.getItem('token')
+          },
+      });
+      
+      if(response.status == 200) {
+            window.location.href = '/login'
+      }
+
+    }
+    catch(error){
+        console.log(error)
+    }
+  }
+
+
 </script>
